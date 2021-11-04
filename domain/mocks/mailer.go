a779acf96d7f49ad87224317dbb45e8f92a5b23d
marksman1298 <marksaid206@gmail.com>
2021-11-04 18:27:24 -0400

    Added tests for sendConfirmation and ConfirmSchoolEnrollment

---
+++ domain/mocks/mailer.go
@@ -0,0 +1,27 @@
+package mocks
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+type SimpleMail struct {
+	from string
+	password string
+	// smtp.gmail.com
+	smtpHost string
+	// 587
+	smtpPort string
+	mock.Mock
+}
+
+func (m SimpleMail) SendSimpleMail(to string, body []byte) error {
+	args := m.Called(to, body)
+
+	var r0 error
+	if rf, ok := args.Get(0).(func(string, []byte) error); ok{
+		r0 = rf(to, body)
+	} else {
+		r0 = args.Error(0)
+	}
+	return r0
+}
