2fa2c6b1ff5282634debe837f98b256eb70c7ade
marksman1298 <marksaid206@gmail.com>
2021-11-07 14:45:21 -0500

    Fixed url for school delivery tests

--- School/delivery/http/school_delivery_test.go
+++ School/delivery/http/school_delivery_test.go
@@ -1,6 +1,7 @@
 package http_test
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/airbenders/profile/School/delivery/http"
@@ -22,7 +23,8 @@ import (
 func TestSchoolHandler_SearchStudentSchool(t *testing.T){
 	mockUseCase := new(mocks.SchoolUseCase)
 	h := http.NewSchoolHandler(mockUseCase)
-	server := httptest.NewServer(app.Server(nil, h))
+	middleware := new(mocks.MiddlewareMock)
+	server := httptest.NewServer(app.Server(nil, h, nil, nil, middleware))
 	defer server.Close()
 
 	var mockSchool domain.School
@@ -35,7 +37,7 @@ func TestSchoolHandler_SearchStudentSchool(t *testing.T){
 			On("SearchSchoolByDomain", mock.Anything, mock.AnythingOfType("string")).
 			Return(arrMockSchool, nil).
 			Once()
-		response, err := server.Client().Get(fmt.Sprintf("%s/school/?domain=test", server.URL))
+		response, err := server.Client().Get(fmt.Sprintf("%s/api/school/?domain=test", server.URL))
 		assert.NoError(t, err)
 		defer response.Body.Close()
 		assert.Equal(t, 200, response.StatusCode)
@@ -52,7 +54,7 @@ func TestSchoolHandler_SearchStudentSchool(t *testing.T){
 	})
 
 	t.Run("case no domain", func(t *testing.T){
-		response, err := server.Client().Get(fmt.Sprintf("%s/school/?domain=", server.URL))
+		response, err := server.Client().Get(fmt.Sprintf("%s/api/school/?domain=", server.URL))
 		assert.NoError(t, err)
 		defer response.Body.Close()
 		assert.Equal(t, 400, response.StatusCode)
@@ -73,7 +75,7 @@ func TestSchoolHandler_SearchStudentSchool(t *testing.T){
 			On("SearchSchoolByDomain", mock.Anything, mock.AnythingOfType("string")).
 			Return(nil, errors.New("error")).
 			Once()
-		response, err := server.Client().Get(fmt.Sprintf("%s/school/?domain=test", server.URL))
+		response, err := server.Client().Get(fmt.Sprintf("%s/api/school/?domain=test", server.URL))
 		defaultErr := errors.New("error")
 		assert.NoError(t, err)
 		defer response.Body.Close()
@@ -92,7 +94,8 @@ func TestSchoolHandler_SearchStudentSchool(t *testing.T){
 func TestSchoolHandler_ConfirmSchoolRegistration(t *testing.T) {
 	mockUseCase := new(mocks.SchoolUseCase)
 	h := http.NewSchoolHandler(mockUseCase)
-	server := httptest.NewServer(app.Server(nil, h))
+	middleware := new(mocks.MiddlewareMock)
+	server := httptest.NewServer(app.Server(nil, h, nil, nil, middleware))
 	defer server.Close()
 
 	var mockSchool domain.School
@@ -149,8 +152,8 @@ func TestSchoolHandler_ConfirmSchoolRegistration(t *testing.T) {
 func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 	mockUseCase := new(mocks.SchoolUseCase)
 	h := http.NewSchoolHandler(mockUseCase)
-
-	server := httptest.NewServer(app.Server(nil, h))
+	middleware := new(mocks.MiddlewareMock)
+	server := httptest.NewServer(app.Server(nil, h, nil, nil, middleware))
 	defer server.Close()
 	var mockSchool *domain.School
 	err := faker.FakeData(&mockSchool)
@@ -163,7 +166,7 @@ func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 		postBody, err := json.Marshal(mockSchool)
 		assert.NoError(t, err)
 		reader := strings.NewReader(string(postBody))
-		response, err := server.Client().Post(fmt.Sprintf("%s/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
+		response, err := server.Client().Post(fmt.Sprintf("%s/api/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
 			"application/JSON", reader)
 		assert.NoError(t, err)
 		defer response.Body.Close()
@@ -178,7 +181,7 @@ func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 
 	t.Run("invalid-body", func(t *testing.T) {
 		reader := strings.NewReader("Invalid body")
-		response, err := server.Client().Post(fmt.Sprintf("%s/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
+		response, err := server.Client().Post(fmt.Sprintf("%s/api/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
 			"application/JSON", reader)
 		assert.NoError(t, err)
 		defer response.Body.Close()
@@ -199,8 +202,8 @@ func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 	t.Run("invalid-email", func(t *testing.T) {
 		mockUseCase := new(mocks.SchoolUseCase)
 		h := http.NewSchoolHandler(mockUseCase)
-		r := app.Server(nil, h)
-		reqFound := httptest.NewRequest("POST", fmt.Sprintf("/school/confirm?email=%s", ""), nil)
+		r := app.Server(nil, h, nil, nil, middleware)
+		reqFound := httptest.NewRequest("POST", fmt.Sprintf("/api/school/confirm?email=%s", ""), nil)
 		w := httptest.NewRecorder()
 		r.ServeHTTP(w, reqFound)
 		assert.Equal(t, 400, w.Code)
@@ -213,7 +216,7 @@ func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 		postBody, err := json.Marshal(mockSchool)
 		assert.NoError(t, err)
 		reader := strings.NewReader(string(postBody))
-		response, err := server.Client().Post(fmt.Sprintf("%s/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
+		response, err := server.Client().Post(fmt.Sprintf("%s/api/school/confirm?email=%s", server.URL, "adam.yafout@gmail.com"),
 			"application/JSON", reader)
 		assert.NoError(t, err)
 		defer response.Body.Close()
@@ -228,4 +231,4 @@ func TestSchoolHandler_SendConfirmationMail(t *testing.T){
 
 
 }
-}
+
