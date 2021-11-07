ef4035562babbaf50df61ae70dbced2904543dbf
Zubair Nurie <mznurie@msn.com>
2021-11-07 15:24:08 -0500

    Refactor: remove the bugs with binding student for no reason

--- domain/mocks/review_repository.go
+++ domain/mocks/review_repository.go
@@ -2,6 +2,7 @@ package mocks
 
 import (
 	"context"
+
 	"github.com/airbenders/profile/domain"
 	"github.com/stretchr/testify/mock"
 )
@@ -42,18 +43,76 @@ func (m *ReviewRepositoryMock) GetReviewsFor(ctx context.Context, reviewed strin
 	return r0, r1
 }
 
-func (r *ReviewRepositoryMock) GetReviewsBy(ctx context.Context, reviewer string) ([]domain.Review, error) {
-	panic("implement me")
+func (m *ReviewRepositoryMock) GetReviewsBy(ctx context.Context, reviewer string) ([]domain.Review, error) {
+	args := m.Called(ctx, reviewer)
+
+	var r0 []domain.Review
+	if rf, ok := args.Get(0).(func(context.Context, string) []domain.Review); ok {
+		r0 = rf(ctx, reviewer)
+	} else {
+		if args.Get(0) != nil {
+			r0 = args.Get(0).([]domain.Review)
+		}
+	}
+
+	var r1 error
+	if rf, ok := args.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, reviewer)
+	} else {
+		r1 = args.Error(1)
+	}
+
+	return r0, r1
 }
 
-func (r *ReviewRepositoryMock) GetReviewByAndFor(ctx context.Context, reviewer string, reviewed string) (*domain.Review, error) {
-	panic("implement me")
+func (m *ReviewRepositoryMock) GetReviewByAndFor(ctx context.Context, reviewer string, reviewed string) (*domain.Review, error) {
+	args := m.Called(ctx, reviewer, reviewed)
+
+	var r0 *domain.Review
+	if rf, ok := args.Get(0).(func(context.Context, string, string) *domain.Review); ok {
+		r0 = rf(ctx, reviewer, reviewed)
+	} else {
+		if args.Get(0) != nil {
+			r0 = args.Get(0).(*domain.Review)
+		}
+	}
+
+	var r1 error
+	if rf, ok := args.Get(1).(func(context.Context, string, string) error); ok {
+		r1 = rf(ctx, reviewer, reviewed)
+	} else {
+		r1 = args.Error(1)
+	}
+
+	return r0, r1
 }
 
-func (r *ReviewRepositoryMock) AddReview(ctx context.Context, review *domain.Review) error {
-	panic("implement me")
+func (m *ReviewRepositoryMock) AddReview(ctx context.Context, review *domain.Review) error {
+	args := m.Called(ctx, review)
+
+	var r0 error
+	if rf, ok := args.Get(0).(func(context.Context, *domain.Review) error); ok {
+		r0 = rf(ctx, review)
+	} else {
+		if args.Get(0) != nil {
+			r0 = args.Get(0).(error)
+		}
+	}
+
+	return r0
 }
 
-func (r *ReviewRepositoryMock) UpdateReviewTags(ctx context.Context, review *domain.Review) error {
-	panic("implement me")
+func (m *ReviewRepositoryMock) UpdateReviewTags(ctx context.Context, review *domain.Review) error {
+	args := m.Called(ctx, review)
+
+	var r0 error
+	if rf, ok := args.Get(0).(func(context.Context, *domain.Review) error); ok {
+		r0 = rf(ctx, review)
+	} else {
+		if args.Get(0) != nil {
+			r0 = args.Get(0).(error)
+		}
+	}
+
+	return r0
 }
--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/jackc/pgx/v4 v4.13.0 // indirect
 	github.com/jackc/puddle v1.1.3 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/junhwi/gobco v0.0.0-20200104144416-c015e3f3de35 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
