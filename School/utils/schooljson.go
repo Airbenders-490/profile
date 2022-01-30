15ae4ebc9974668fb70aeb9064b9c1c3edae41ab
Adam Yafout <adam.yafout@gmail.com>
2022-01-29 21:59:04 -0500

    STUD-191, potential fix

--- School/utils/schooljson.go
+++ School/utils/schooljson.go
@@ -12,12 +12,13 @@ import (
 
 func createJSONFile() {
 	resp, _ := http.Get("https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json%22")
-	schoolJSON, _ := ioutil.ReadAll(resp.Body)
-	// The os.Create creates or truncates the named file. If the file already exists, it is truncated.
-	jsonFile,_ := os.Create("school.json")
-	data := []byte(schoolJSON)
-	_, err2 := jsonFile.Write(data)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
+	//schoolJSON, _ := ioutil.ReadAll(resp.Body)
+	//// The os.Create creates or truncates the named file. If the file already exists, it is truncated.
+	//jsonFile,_ := os.Create("school.json")
+	//data := []byte(schoolJSON)
+	//_, err2 := jsonFile.Write(data)
+	//if err2 != nil {
+	//	log.Fatal(err2)
+	//}
+
 }
