6d46776f6e0df9779d954d787652e439257051b8
Zubair Nurie <45270163+nuzurie@users.noreply.github.com>
2022-02-24 14:45:07 -0500

    Update student_create.sql

--- Student/repository/student_create.sql
+++ Student/repository/student_create.sql
@@ -1,17 +1,30 @@
+-- Table: public.student
+
+-- DROP TABLE public.student;
+
 CREATE TABLE IF NOT EXISTS public.student
 (
     id character varying(64) COLLATE pg_catalog."default" NOT NULL,
     first_name character varying(64) COLLATE pg_catalog."default",
     last_name character varying(64) COLLATE pg_catalog."default",
     email character varying(64) COLLATE pg_catalog."default",
-    general_info character varying(1024) COLLATE pg_catalog."default",
-    school character varying(64) COLLATE pg_catalog."default",
     current_classes text[] COLLATE pg_catalog."default",
     classes_taken text[] COLLATE pg_catalog."default",
+    general_info character varying(1024) COLLATE pg_catalog."default",
+    school character varying(64) COLLATE pg_catalog."default",
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
-    CONSTRAINT student_pkey PRIMARY KEY (id)
-);
+    CONSTRAINT student_pkey PRIMARY KEY (id),
+    CONSTRAINT school_pkey FOREIGN KEY (school)
+        REFERENCES public.school (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.student
+    OWNER to postgres;
 
 INSERT INTO public.student (id, first_name, last_name, email, general_info, created_at, updated_at)
 VALUES ('234', 'Also Zubair', 'Nurie', 'mzznurie@msn.com', 'ballerr', now(), now());
